5794ed7bf8e2c27db07cbccb9a8c85be62cb1f5e
ishika <imusun24@gmail.com>
2021-03-13 01:32:06 +0530

    yes im studying

---
+++ 6- multiple class/multiple.java
@@ -0,0 +1,19 @@
+class Example{
+    int a,b,total=0;
+    void add()
+    {
+        total = a+b;
+    }
+}
+
+public class multiple {
+    public static void main (String a[])
+    {
+        Example obj = new Example();
+        obj.a = 10;
+        obj.b = 20;
+        obj.add();
+        System.out.println("Addition=" + obj.total);
+    }
+    
+}
